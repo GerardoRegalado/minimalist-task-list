415751e7b07be0c07ce8b4b87429f408cbc5eece
Gerardo Regalado <gerardo.regalado24@gmail.com>
2023-11-29 13:35:28 -0600

    Feat(): Removing default data and creating the structure for the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./components/header/header.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { MainComponent } from "./components/main/main.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports :[ RouterOutlet, HeaderComponent, FooterComponent, MainComponent]
 })
 export class AppComponent {
   title = 'angular-task-list';
